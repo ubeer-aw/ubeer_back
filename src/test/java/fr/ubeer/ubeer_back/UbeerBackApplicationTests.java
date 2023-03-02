83f3c627b38d75f386e8d8d4aa666b3ed5add011
Arthur HEUDE <arthur.heude@laposte.net>
2023-03-02 11:09:28 +0100

    init

---
+++ src/test/java/fr/ubeer/ubeer_back/UbeerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ubeer.ubeer_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbeerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
